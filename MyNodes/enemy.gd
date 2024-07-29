6cbc8d4ded6f86668dc8428519df0018d3345d5d
unknown <nicolas.saucedo@adesn.org.ar>
2024-07-29 12:54:23 -0300

    no se q hice

--- MyNodes/enemy.gd
+++ MyNodes/enemy.gd
@@ -5,6 +5,8 @@ var collider
 var SPEED = 300
 var direction;
 
+var firstQuit = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	collider = $CharacterBody2D/CollisionShape2D
@@ -16,8 +18,14 @@ func _process(delta):
 	#position = lerp(position, personaje.position, SPEED*delta)
 	#print((position - personaje.position).normalized())
 	position += direction * SPEED * delta
-
-	
 	
 
-
+func _on_area_2d_body_exited(body):
+	if(body.name == 'TileMap'):
+		if (firstQuit):
+			firstQuit = !firstQuit
+			print("priemrsalida")
+		else:
+			print("borrado")
+			queue_free()
+	pass # Replace with function body.
--- MyNodes/personaje.gd
+++ MyNodes/personaje.gd
@@ -10,29 +10,34 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-		# Get the input direction and handle the movement/deceleration.
+	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionX = Input.get_axis("ui_left", "ui_right")
+	velocity.x = directionX * SPEED
 	if directionX:
-		position.x += directionX * SPEED * delta
 		if directionX > 0:
 			anim_pj(3)
 		else:
 			anim_pj(2)
-	
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
 	var directionY = Input.get_axis("ui_up", "ui_down")
 	if directionY:
-		position.y += directionY * SPEED * delta
+		velocity.y = directionY * SPEED
 		if directionY > 0:
 			anim_pj(0)
 		else:
 			anim_pj(1)
-
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
 
 
 func anim_pj(myframe):
 	#animando personaje
-	print(myframe)
 	$Idle.frame = myframe
 
 func perder_vida():
